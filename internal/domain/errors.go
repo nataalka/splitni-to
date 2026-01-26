6768355936016f65e7136a0e6b3828c299ef31dc
Natalia Ligacova <x539936@fi.muni.cz>
2026-01-26 19:54:11 +0100

    feat: custom errors

---
+++ internal/domain/errors.go
@@ -0,0 +1,62 @@
+package domain
+
+import (
+	"fmt"
+)
+
+type AppError struct {
+	Type    ErrorType
+	Message string
+	Err     error
+}
+
+func (e *AppError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %s (%v)", e.Type, e.Message, e.Err)
+	}
+	return fmt.Sprintf("%s: %s", e.Type, e.Message)
+}
+
+func (e *AppError) Unwrap() error {
+	return e.Err
+}
+
+func (e *AppError) Wrap(err error) *AppError {
+	e.Err = err
+	return e
+}
+
+type ErrorType string
+
+const (
+	TypeValidation ErrorType = "VALIDATION_ERROR"
+	TypeNotFound   ErrorType = "NOT_FOUND"
+	TypeConflict   ErrorType = "CONFLICT"
+	TypeInternal   ErrorType = "INTERNAL_ERROR"
+	TypeAuth       ErrorType = "UNAUTHORIZED"
+)
+
+func NewValidationError(msg string) *AppError {
+	return &AppError{Type: TypeValidation, Message: msg}
+}
+
+func NewNotFoundError(msg string) *AppError {
+	return &AppError{Type: TypeNotFound, Message: msg}
+}
+
+func NewConflictError(msg string) *AppError {
+	return &AppError{Type: TypeConflict, Message: msg}
+}
+
+func NewInternalError(msg string, err error) *AppError {
+	return &AppError{Type: TypeInternal, Message: msg, Err: err}
+}
+
+func NewAuthError(msg string) *AppError {
+	return &AppError{Type: TypeAuth, Message: msg}
+}
+
+var (
+	ErrEmailTaken   = NewConflictError("email is already registered")
+	ErrUserNotFound = NewNotFoundError("user not found")
+)
