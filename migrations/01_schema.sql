f7ac6af8a78b8dea390dfd2d1c6b83d121c82bfb
Natalia Ligacova <x539936@fi.muni.cz>
2026-01-26 19:22:10 +0100

    fix: use uuid in db schema

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,33 +1,35 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users
 (
-    id            SERIAL PRIMARY KEY,
+    id            UUID                NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
     name          VARCHAR(100)        NOT NULL,
     surname       VARCHAR(100)        NOT NULL,
     email         VARCHAR(100) UNIQUE NOT NULL,
     password_hash TEXT,
-    created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at    TIMESTAMP WITH TIME ZONE     DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE groups
 (
-    id         SERIAL PRIMARY KEY,
+    id         UUID         NOT NULL    DEFAULT uuid_generate_v4() PRIMARY KEY,
     name       VARCHAR(100) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE group_members
 (
-    group_id  INTEGER REFERENCES groups (id) ON DELETE CASCADE,
-    user_id   INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    group_id  UUID REFERENCES groups (id) ON DELETE CASCADE,
+    user_id   UUID REFERENCES users (id) ON DELETE CASCADE,
     joined_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (group_id, user_id)
 );
 
 CREATE TABLE expenses
 (
-    id          SERIAL PRIMARY KEY,
-    group_id    INTEGER REFERENCES groups (id) ON DELETE CASCADE,
-    payer_id    INTEGER        REFERENCES users (id) ON DELETE SET NULL,
+    id          UUID           NOT NULL  DEFAULT uuid_generate_v4() PRIMARY KEY,
+    group_id    UUID REFERENCES groups (id) ON DELETE CASCADE,
+    payer_id    UUID           REFERENCES users (id) ON DELETE SET NULL,
     amount      DECIMAL(12, 2) NOT NULL,
     description TEXT           NOT NULL,
     created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
@@ -35,8 +37,8 @@ CREATE TABLE expenses
 
 CREATE TABLE expense_splits
 (
-    expense_id INTEGER REFERENCES expenses (id) ON DELETE CASCADE,
-    user_id    INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    expense_id UUID REFERENCES expenses (id) ON DELETE CASCADE,
+    user_id    UUID REFERENCES users (id) ON DELETE CASCADE,
     amount     DECIMAL(12, 2) NOT NULL,
     PRIMARY KEY (expense_id, user_id)
 );
